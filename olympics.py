3cd8d9d1341c719fe1fb9f68b2fed1d994de9378
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-03-07 08:40:18 -0500

    Updated from Brython Server: 3/7/2018 8:40:18 AM

--- olympics.py
+++ olympics.py
@@ -1,3 +1,24 @@
 #Christo Dragnev
 #3/7/18
-#/
+#olympics.py
+
+red = Color(0xFF0000,1) #this is the color red
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+
+blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, full
+blackCircle = CircleAsset(50,blackOutline,black) #radius, outline, full
+redCircle = CircleAsset(50,blackOutline,red) #radius, outline, full
+yellowCircle = CircleAsset(50,blackOutline,yellow) #radius, outline, full
+greenCircle = CircleAsset(50,blackOutline,green) #radius, outline, full
+
+Sprite(blueCircle,(50,50))
+Sprite(blackCircle,(100,50))
+Sprite(redCircle,(150,50))
+Sprite(yellowCircle,(75,75))
+Sprite(greenCircle,(125,75))
+
+App().run()
+
